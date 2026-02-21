3693da3a0b0e19561cdb9a2285133daa0e3adb7a
QikDev <vokounvojta03@gmail.com>
2026-02-21 18:01:33 +0100

    Update vnc.bat

--- vnc.bat
+++ vnc.bat
@@ -2,7 +2,7 @@
 set "NO_VNC_DIR=C:\ProgramData\server\noVnc"
 
 :: Start TightVNC server hidden
-start "" "C:\Program Files\TightVNC\tvnserver.exe"
+start "tight" "C:\Program Files\TightVNC\tvnserver.exe"
 
 :: Wait a few seconds for TightVNC to initialize
 timeout /t 5 /nobreak >nul
@@ -15,3 +15,4 @@ timeout /t 5 /nobreak >nul
 
 :: Run cloudflared tunnel hidden
 powershell -WindowStyle Hidden -Command "Start-Process cloudflared -ArgumentList 'tunnel run --token eyJhIjoiMzJkOThkNTA1ZmI1OTE4ODhiNjAzYWU1Y2EyYzFiNjUiLCJzIjoiTVdKa01qVmlOREl0TW1KbE1pMDBPVFF5TFdGaFltVXRNVFZtT0Rka01XVXdNalF6IiwidCI6IjJkMjRjMThmLTg5NTQtNGM4Yy05YTVkLWNjMzIxYTNkZjRmZCJ9' -WindowStyle Hidden"
+
